e13809389632b0dbc2df06a035896415265eb558
Mzanuso <mzanuso@gmail.com>
2025-03-10 19:31:09 -0400

    Aggiunta file __init__.py per il package schemas

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,1 +1,18 @@
-# Schemas package initialization
+from .users import User, UserCreate, UserUpdate, UserInDB
+from .projects import Project, ProjectCreate, ProjectUpdate, ProjectInDB, StyleData, Screenplay, StoryboardImage, Production
+from .styles import Style, StyleCreate, StyleUpdate, StyleInDB, StyleCategory
+from .media import Media, MediaCreate, MediaUpdate, MediaInDB, MediaUpload, MediaEdit, MediaExport
+
+# For convenience, export all models
+__all__ = [
+    # Users
+    "User", "UserCreate", "UserUpdate", "UserInDB",
+    # Projects
+    "Project", "ProjectCreate", "ProjectUpdate", "ProjectInDB",
+    "StyleData", "Screenplay", "StoryboardImage", "Production",
+    # Styles
+    "Style", "StyleCreate", "StyleUpdate", "StyleInDB", "StyleCategory",
+    # Media
+    "Media", "MediaCreate", "MediaUpdate", "MediaInDB",
+    "MediaUpload", "MediaEdit", "MediaExport"
+]
